6ffe295ae2abc331e533fce34ddf81d4c21410a5
zulfidly <114971952+zulfidly@users.noreply.github.com>
2022-10-05 18:51:54 +0800

    Add files via upload

---
+++ AgeToDays.js
@@ -0,0 +1,10 @@
+// https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw
+
+function calcAge(age) {
+	days=age*365
+    return(days)
+}
+console.log(calcAge(65))
+console.log(calcAge(0))
+console.log(calcAge(20))
+
