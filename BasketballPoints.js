76b73d25e2b2a0b6ae8e1e157f0c35d63d648e4c
zulfidly <114971952+zulfidly@users.noreply.github.com>
2022-10-14 15:39:45 +0800

    Delete BasketballPoints.js

--- BasketballPoints.js
+++
@@ -1,9 +0,0 @@
-// https://edabit.com/challenge/Y46Xp2pcvTB77bmdD
-
-function points(twoPointers, threePointers) {
-	Pointers=twoPointers*2 + threePointers*3
-    return (Pointers)
-}
-console.log(points(1, 1))
-console.log(points(7, 5))
-console.log(points(38, 8))
