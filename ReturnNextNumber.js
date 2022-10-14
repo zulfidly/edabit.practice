29664d02e4f5b59680839c7f907fdbea63c054b4
zulfidly <114971952+zulfidly@users.noreply.github.com>
2022-10-14 15:39:55 +0800

    Delete ReturnNextNumber.js

--- ReturnNextNumber.js
+++
@@ -1,9 +0,0 @@
-// https://edabit.com/challenge/NAQhEoxbofPidLxm9
-
-function addition(num) {
-	X=num+1
-    return(X)
-}
-console.log(addition(0))
-console.log(addition(9))
-console.log(addition(-3))
